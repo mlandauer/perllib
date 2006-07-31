2688b664f2c8f9c5745a09c826ea1c78d0e0ba01
chris <chris>
2006-07-31 13:12:40 +0000

    Hmm.

--- mySociety/Web.pm
+++ mySociety/Web.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: Web.pm,v 1.9 2006/07/21 13:42:27 chris Exp $
+# $Id: Web.pm,v 1.10 2006/07/31 13:12:40 chris Exp $
 #
 
 package mySociety::Web;
@@ -189,8 +189,9 @@ that the parameter should be removed in the new URL.
 =cut
 sub NewURL ($%) {
     my ($q, %p) = @_;
-    my $url = $q->url(-absolute => 1);
     my @v = ();
+    my $q = $self->q();
+    my $url = $q->url(-absolute => 1);
     foreach my $key ($q->param()) {
         if (exists($p{$key})) {
             next if (!defined($p{$key}));
